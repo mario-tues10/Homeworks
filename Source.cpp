e4414b05395b460bbceec79352a0cd2be549a548
mariosexa13 <37478097+mariosexa13@users.noreply.github.com>
2019-04-15 21:45:37 +0300

    Homework
    Homework - second task.

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include <cstdio>
 using namespace std;
-double square_root(double S, int n) {
-	double x0 = S;
-	for (int i = 0; i < n; i++) {
-		x0 = (x0 + (S / x0)) / 2;
-	}
-	return x0;
+int c = 0;
+int incrementation() {
+	c++;
+	return c;
 }
-
-int main()
-{
-	double number = 125348;
-	printf("%f\n", square_root(number, 20));
+int main() {
+	printf("%d\n", incrementation());
+	printf("%d\n", incrementation());
+	printf("%d\n", incrementation());
 
 	return 0;
 }
