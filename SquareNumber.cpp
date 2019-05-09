04457b1fad22eb15e77f8f8d52fef5e4ee2a458f
mariosexa13 <37478097+mariosexa13@users.noreply.github.com>
2019-05-09 22:49:39 +0300

    Add files via upload

---
+++ SquareNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+double square_root(double S, int n) {
+	double x0 = S;
+	for (int i = 0; i < n; i++) {
+		x0 = (x0 + (S / x0)) / 2;
+	}
+	return x0;
+}
+int main()
+{
+	double number = 125348;
+	printf("%f\n", square_root(number, 20));
+	return 0;
+}
