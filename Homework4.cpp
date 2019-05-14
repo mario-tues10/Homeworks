ba9b641ed2c1f2116282b075c6e090f4151b2353
mariosexa13 <37478097+mariosexa13@users.noreply.github.com>
2019-05-14 20:54:48 +0300

    Add files via upload

--- Homework4.cpp
+++ Homework4.cpp
@@ -1,34 +1,61 @@
 #include <cstdio>
 
+
+
 using namespace std;
 
-int secondNum(int t[], int size){
-    int max= t[0];
-    for(int i = 0; i <= size; i++){
-        if(max < t[i]){
-            max = t[i];
-        }
-    }
-    int diff = max - t[0];
-    if(diff == 0){
-        diff = max - t[1];
-    }
-    int secN = max;
-    for(int j = 0; j < size; j++){
-        if(t[j] == max) continue;
-        if((max - t[j]) <= diff){
-            secN = t[j];
-            diff= max - t[j];
-        }
-    }
-   return secN;
+
+
+int secondNum(int t[], int size) {
+
+	int max = t[0];
+
+	for (int i = 0; i <= size; i++) {
+
+		if (max < t[i]) {
+
+			max = t[i];
+
+		}
+
+	}
+
+	int secN = max;
+	
+	int diff = max - t[0];
+		
+	for (int j = 0; j < size; j++) {
+
+		if (t[j] == max) continue;
+
+
+
+		if (((max - t[j]) < diff) || diff == 0) {
+
+			secN = t[j];
+
+			diff = max - t[j];
+
+		}
+
+	}
+
+	return secN;
+
 }
 
+
+
 int main()
+
 {
-    //int *a1, *b1;
-    int t1[] = {15,98,747,423,453,44};
-    int size = sizeof(t1)/sizeof(int);
-    printf("%d ", secondNum(t1,size));
-    return 0;
+
+	int t1[] = { 747,15,98,423,453,44 };
+
+	int size = sizeof(t1) / sizeof(int);
+
+	printf("%d ", secondNum(t1, size));
+
+	return 0;
+
 }
